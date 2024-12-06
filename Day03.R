3c4e48645724b69f73050c9fce402722bb15ec1a
Lyesin <Yan.Lyesin@bci.ca>
2024-12-05 16:16:41 -0800

    progress on day 3 part 2

--- Day03.R
+++ Day03.R
@@ -52,3 +52,22 @@ FROM
 DAY_3_RESULT_1 <- dbGetQuery(conn = con, statement = query_1)
 
 cat(DAY_3_RESULT_1$RESULT)
+
+
+query_2 <- "
+
+WITH
+  UNNESTED AS (
+    SELECT
+      UNNEST(
+        REGEXP_EXTRACT_ALL (TEXT, '((do\\(\\)|don.*?t\\(\\)).*?)?mul\\(\\d{1,3},\\d{1,3}\\)')
+      ) as TEXT_SPLIT
+    FROM
+      AOC_2024_DAY_3
+  )
+
+SELECT * FROM UNNESTED
+
+"
+
+DAY_3_RESULT_2 <- dbGetQuery(conn = con, statement = query_2)
